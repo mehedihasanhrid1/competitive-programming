8bb365239868eee6f4169faf37ea5a02e545b6f2
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-02-06 20:42:58 +0600

    Simple calculator by python

--- Python/02_Calculator.py
+++ Python/02_Calculator.py
@@ -2,5 +2,12 @@ a = int(input("Input a: "))
 b = int(input("Input b: "))
 
 c = a+b
+d = a-b
+e = a*b
+f = a/b
 
 print("Result: " , c)
+print("Result: " , d)
+print("Result: " , e)
+print("Result: " , f)
+
