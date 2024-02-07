ecf1d7184ef706011c9f265ee741baec8ac3c4f2
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-02-07 21:56:53 +0600

    Python quiz

---
+++ Python/03_Quiz.py
@@ -0,0 +1,69 @@
+print("Welcome to the GK Quiz")
+
+print("|What is the capital of France?|")
+
+print("|Please select your option|")
+
+print("| 1. London         |")
+print("| 2. Paris          |")
+print("| 3. Rome           |")
+
+choice = input("Enter 1, 2, or 3:")
+
+if choice == "1":
+    print("Wrong answer.")
+
+elif choice == "2":
+    print("That's the correct answer.")
+
+elif choice == "3":
+    print("Wrong answer.")
+
+else:
+    print("Incorrect option.")
+
+print("|Who wrote 'To Kill a Mockingbird'?|")
+
+print("|Please select your option|")
+
+print("| 1. J.K. Rowling          |")
+print("| 2. Harper Lee            |")
+print("| 3. Ernest Hemingway      |")
+
+choice = input("Enter 1, 2 or 3:")
+
+if choice == "1":
+    print("Wrong answer.")
+
+elif choice == "2":
+    print("That's the correct answer.")
+
+elif choice == "3":
+    print("Wrong answer.")
+
+else:
+    print("Incorrect option.")
+
+print("|What is the chemical symbol for gold?|")
+
+print("|Please select your option|")
+
+print("| 1. Au             |")
+print("| 2. Ag             |")
+print("| 3. Fe             |")
+
+choice = input("Enter 1, 2 or 3:")
+
+if choice == "1":
+    print("That's the correct answer.")
+
+elif choice == "2":
+    print("Wrong answer.")
+
+elif choice == "3":
+    print("Wrong answer.")
+
+else:
+    print("Incorrect option.")
+
+print("Keep your mind sharp and stay curious! See you at the next quiz adventure!")
