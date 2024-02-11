367e9c813f28b7db8c4e4bc17aac29cb8c8eb675
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-02-11 19:09:21 +0600

    Area of Circle In C

---
+++ C/04_Area_of_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define PI 3.14159;
+int main()
+{
+	float r,area;
+	printf("Enter the redious: ");
+	scanf("%f",&r);
+	area=3.1459*r*r;
+	printf("Result = %0.2f",area);
+	return 0;
+}
