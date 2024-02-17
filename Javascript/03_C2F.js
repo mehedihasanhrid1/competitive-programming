826d1e0cd5713547c55503bb0afecda780c33dc2
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-02-17 22:27:49 +0600

    Celsius to fahrenheit in JS

---
+++ Javascript/03_C2F.js
@@ -0,0 +1,10 @@
+
+function cToF(c) {
+    var f = (c * 9/5) + 32;
+    return f;
+}
+
+
+var celsius = 30;
+var fahrenheit = cToF(celsius);
+console.log(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
