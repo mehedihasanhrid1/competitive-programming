28bb493dee8461dbf97dd709fb9e3887caf065ff
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2024-02-22 23:34:31 +0600

    For loop in C++

---
+++ C++/04_For_N.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int N;
+    std::cout << "Enter the value of N: ";
+    std::cin >> N;
+
+    for (int i = 1; i <= N; i++) {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
